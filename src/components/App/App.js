8ca833ab39acb3e0919ad735b2e9c6e37163a08f
Palak Jain <84609623+TechGeek3@users.noreply.github.com>
2022-09-17 18:46:05 +0530

    Add files via upload

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+
+function App() {
+
+  return (
+
+    <></>
+
+  );
+
+}
+
+export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import "./Login.css";
+
+class FluidInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            focused: false,
+            value: ""
+        };
+    }
+    focusField() {
+        const { focused } = this.state;
+        this.setState({
+            focused: !focused
+        });
+    }
+    handleChange(event) {
+        const { target } = event;
+        const { value } = target;
+        this.setState({
+            value: value
+        });
+    }
+    render() {
+        const { type, label, style, id } = this.props;
+        const { focused, value } = this.state;
+
+        let inputClass = "fluid-input";
+        if (focused) {
+            inputClass += " fluid-input--focus";
+        } else if (value !== "") {
+            inputClass += " fluid-input--open";
+        }
+
+        return (
+            <div className={inputClass} style={style}>
+                <div className="fluid-input-holder">
+
+                    <input
+                        className="fluid-input-input"
+                        type={type}
+                        id={id}
+                        onFocus={this.focusField.bind(this)}
+                        onBlur={this.focusField.bind(this)}
+                        onChange={this.handleChange.bind(this)}
+                        autocomplete="off"
+                    />
+                    <label className="fluid-input-label" forHtml={id}>{label}</label>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+class Button extends React.Component {
+    render() {
+        return (
+            <div className={`button ${this.props.buttonClass}`} onClick={this.props.onClick}>
+                {this.props.buttonText}
+            </div>
+        );
+    }
+}
+
+class Login extends React.Component {
+    render() {
+
+        const style = {
+            margin: "15px 0"
+        };
+        return (
+            <div className="login-container">
+                <div className="title">
+                    Login
+                </div>
+                <FluidInput type="text" label="name" id="name" style={style} />
+                <FluidInput type="phone" label="number" id="number" style={style} />
+                <FluidInput type="phone" label="OTP" id="otp" style={style} />
+                <Button buttonText="log in" buttonClass="login-button" />
+            </div>
+        );
+    }
+}
+
+export default Login;
