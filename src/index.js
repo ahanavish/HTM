3a1fd0c5293911ea9309110b2eeedc28c38021ca
Palak Jain <palakjn32@gmail.com>
2022-09-18 15:03:30 +0530

    Login corrected

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Login from "./components/Login/Login";
 // import MedicineDetails from './components/MedicineDetails/MedicineDetails';
-import App from './components/App/App';
+// import App from './components/App/App';
+import Login from './components/Login/Login';
 // import Otp from './components/Otp/Otp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Otp /> */}
-    <App />
+    <Login />
+    {/* <App /> */}
     {/* <MedicineDetails /> */}
   </React.StrictMode>
 );
